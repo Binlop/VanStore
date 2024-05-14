2433b4cf5d947aeb29415b7b926d337d3e13c16c
nock <ladrek13@mail.ru>
2024-05-14 15:49:46 +0700

    resolve problems backend_3

--- backend/apps/users/serializer.py
+++ backend/apps/users/serializer.py
@@ -1,31 +1,35 @@
-from rest_framework.response import Response
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated
-from .serializer import ProfileSerializer
-from rest_framework.views import APIView
-from rest_framework import status
-
-from .serializer import CustomTokenObtainPairSerializer
+from rest_framework import serializers
+from .models import Profile, User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    serializer_class = CustomTokenObtainPairSerializer
-
-@api_view(['GET'])
-@permission_classes([IsAuthenticated])
-def get_profile(request):
-    user = request.user
-    profile = user.profile
-    serializer = ProfileSerializer(profile)
-    return Response(serializer.data)
+from django.contrib.auth.validators import UnicodeUsernameValidator
+from rest_framework.validators import UniqueValidator
+from django.core.validators import RegexValidator
+from .services.user_process import UserService
 
 
-class RegisterView(APIView):
+class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['username'] = user.username
+        return token
+    
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+        extra_kwargs = {"password": {"write_only": True}}
 
-    def post(self, request):
-        serializer = ProfileSerializer(data=request.data)        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    username = serializers.CharField(validators=[RegexValidator(r'^[\w.@+-]+$', 'Введите корректное имя пользователя. Можете использовать буквы, цифры и символы @/./+/-/_'), UniqueValidator(queryset=User.objects.all(), message='Это имя пользователя уже занято.')])
+    email = serializers.EmailField(validators=[UniqueValidator(queryset=User.objects.all(), message='Этот адрес электронной почты уже зарегистрирован.')])
+
+    class Meta:
+        model = Profile
+        fields = ('user', 'first_name', 'last_name', 'username', 'email', 'is_manager')
+
+    def create(self, validated_data: dict) -> Profile:
+        service = UserService()
+        return service.create_user(validated_data=validated_data)
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -1,9 +1,10 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
-from .serializer import RegistrationSerializer
+from .serializer import ProfileSerializer
 from rest_framework.views import APIView
 from rest_framework import status
+
 from .serializer import CustomTokenObtainPairSerializer
 from rest_framework_simplejwt.views import TokenObtainPairView
 
@@ -15,32 +16,16 @@ class CustomTokenObtainPairView(TokenObtainPairView):
 def get_profile(request):
     user = request.user
     profile = user.profile
-    serializer = RegistrationSerializer(profile)
+    serializer = ProfileSerializer(profile)
     return Response(serializer.data)
 
 
 class RegisterView(APIView):
 
     def post(self, request):
-        print(request.data)
-        serializer = RegistrationSerializer(data=request.data)        
+        serializer = ProfileSerializer(data=request.data)        
         if serializer.is_valid():
             serializer.save()
-            return Response({'message': 'Письмо отправлено на указанную почту. Подтвердите свою учетную запись.'}, status=status.HTTP_201_CREATED)
-                
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-
-class ConfirmAccountView(APIView):
-    def get(self, request, token):
-        try:
-            user = Account.objects.get(confirmation_token=token)
-        except Account.DoesNotExist:
-            return Response({'error': 'Invalid confirmation token'}, status=status.HTTP_400_BAD_REQUEST)
-
-        user.is_active = True
-        user.confirmation_token = None
-        user.save()
-
-        return Response({'message': 'Учетная запись подтверждена успешно'})
-
